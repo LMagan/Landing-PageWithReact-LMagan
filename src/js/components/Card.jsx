cfb29dc5cad3db8a8def9f3750a9f8be901ff4d3
L.Magán <lucasfliper@hotmail.com>
2024-12-12 08:33:28 +0000

    second-commit

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Card = (props) => {
     return (
-        <div className="card" style={{width: '18rem'}}>
+        <div className="card m-2" style={{width: '18rem'}}>
             <img src={props.imageUrl} className="card-img-top"  alt="some img" />
             <div className="card-body">
                 <h5 className="card-title">Más Memes</h5>
--- src/js/components/Gallery.jsx
+++ src/js/components/Gallery.jsx
@@ -6,17 +6,17 @@ const Gallery = () => {
     return (
         <div class="album py-5 bg-body-tertiary">
             <div class="container">
-                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4">
-                    <div class="col">
+                <div class="row ">
+                    <div class="col m-2">
                         <Card imageUrl="https://materiageek.com/wp-content/uploads/2022/12/Estos-memes-de-Android-alegraran-tu-dia.jpg"></Card>
                     </div>
-                    <div class="col">
+                    <div class="col m-2">
                         <Card imageUrl="https://s1.elespanol.com/2016/09/27/ciencia/tecnologia/tecnologia_158745409_17463576_1706x1280.jpg"></Card>
                     </div>
-                    <div class="col">
+                    <div class="col m-2">
                         <Card imageUrl="https://i.pinimg.com/736x/52/79/69/52796911865e118118f45c68417bbde6.jpg"></Card>
                     </div>
-                    <div class="col">
+                    <div class="col m-2">
                         <Card imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRc-8pBw5_nzb4ATuAc8DULDCI4Ob5aUTm73UxmNEg9D07L7ckeuq1CPhjlc9s-0Xc8ODM&usqp=CAU"></Card>
                     </div>
                   
